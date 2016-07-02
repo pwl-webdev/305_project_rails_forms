ba580cf87b6179ed14e66e8d455a212e9e1b1bd2
Pwl Webdev <pwl.webdev@gmail.com>
2016-07-02 22:54:49 +0200

    Created form for editing user

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -13,8 +13,21 @@ class UsersController < ApplicationController
       end
     end
 
+    def edit
+    	@user = User.find(params[:id])
+    end
+
+    def update
+    	@user = User.find(params[:id])
+    	if @user.update(user_params)
+    		redirect_to edit_user_path(@user)
+    	else
+    		render :edit
+    	end
+    end
+
     private
     def user_params
-    	params.require(:user).permit(:username,:email,:password)
+    	params.require(:user).permit(:username,:email,:password,:id)
     end
 end
